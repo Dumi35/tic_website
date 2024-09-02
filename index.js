683d4ce6a363d5f2665e4e5e13ae514150e262b7
Dumebi35 <71632162+Dumi35@users.noreply.github.com>
2024-09-02 11:58:44 +0100

    feat (why-join-us-section): animate fade in for cards

--- index.js
+++ index.js
@@ -2,7 +2,37 @@
 function scrollToElement(elementId) {
     var element = document.getElementById(elementId);
     element.scrollIntoView({
-      block: 'start',
+        block: 'nearest',
     });
-  }
-  
+}
+
+/* Animate on reaching element */
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        const square = entry.target.querySelectorAll('*');
+
+        square.forEach(element => {
+            const value = element.getAttribute("data-animation")
+
+            if (entry.isIntersecting) {
+                element.classList.add(`${value}`);
+                return; // if we added the class, exit the function
+            }
+
+            // We're not intersecting, so remove the class!
+            element.classList.remove(`${value}`);
+        })
+    });
+});
+
+
+const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+
+//do not animate if user's preference is reduced motion
+if (!prefersReducedMotion) {
+    // Select and observe all elements with the 'why-join-us-section__card' class
+    document.querySelectorAll('.why-join-us-section__card').forEach(element => {
+        observer.observe(element);
+    });
+}
+
