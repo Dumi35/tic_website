af626ebe39abbc5518ed2d8978e050da9e7a5e08
Dumebi35 <71632162+Dumi35@users.noreply.github.com>
2024-10-14 17:01:28 +0100

    fix(events): pagination styling

--- events.js
+++ events.js
@@ -1,16 +1,12 @@
 const swiper = new Swiper('.swiper-container', {
     loop: true,
     autoplay: {
-      delay: 3000, // Auto slide every 3 seconds
+      delay: 5000, // Auto slide every 3 seconds
       disableOnInteraction: false,
     },
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
     },
     grabCursor: true,
     slidesPerView: 1, // Shows one slide at a time
